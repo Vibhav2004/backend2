e8a9b0ffdf932bf52b9e0bdd19edd1772d12d2dc
Vibhav <vibhavkhichi13@gmail.com>
2026-03-15 12:23:30 +0530

    new db

--- src/main/java/com/swipenow/swipenow/service/SupaBaseService.java
+++ src/main/java/com/swipenow/swipenow/service/SupaBaseService.java
@@ -29,7 +29,7 @@ public class SupaBaseService {
 
     // Use service_role key for uploads
     private final String SUPABASE_URL = "https://marjfmmbeuektmtvjkiq.supabase.co";
-    private final String SUPABASE_SERVICE_ROLE_KEY = "sb_secret_ya5Jv1dpnmtYWKkbmB2uyA_C34CnOuF";
+    private final String SUPABASE_SERVICE_ROLE_KEY = "s";
     private final String BUCKET = "MEME";
 
     public SupaBaseService(MemeRepo memeRepository) {
